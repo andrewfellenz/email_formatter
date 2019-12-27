66d8d5f7b99d4fb6559798b31bfdf4295e4a2417
Drew <andrewfellenz@gmail.com>
2019-12-27 12:39:52 -0500

    cleaned up a bit

--- email_formatter.c
+++ email_formatter.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+// still need to check for windows vs linux OS
+#include <stdio.h> // default library
+#include <stdlib.h> //stdlib is required to use malloc
 
 void main(int argc, char *argv[])
 {
   FILE *fptr;
-  fptr = fopen("./test_template1.txt", "r");
+  fptr = fopen(argv[1]z, "r");
 
   // determine file size and set 
   char fsize;
   fseek(fptr, 0L, SEEK_END);
   fsize = ftell(fptr);
   rewind(fptr);
-  char *email = malloc(fsize + 100);
+  char *email = malloc(fsize + 1);
 
   fread(email, 1, fsize, fptr);
-  printf(email, argv[1], argv[2], argv[3]);
+  printf(email, argv[2], argv[3], argv[4]);
 
   fclose(fptr);
 }
